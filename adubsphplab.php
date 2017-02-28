74ba5fb2084777b429b1750bd123b095d9fccaec
olukoredebanjo <1614132@rgu.ac.uk>
2017-02-28 17:18:48 +0000

    CHANGES MADE TO CODES @ 5:11PM

---
+++ adubsphplab.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body>
+<p>
+    <?php
+    echo "hello world";
+    // i don't! i'm a comment.
+
+    /* i don't get printed either
+    and neither do i */
+    $name = "Edgar";
+
+    if ($name == "simon") {
+        print " i know you!";
+    }
+
+    else
+    {
+        print "Who are You?";
+    }
+
+
+$wantedGood = "mugs";
+
+switch ($numberOfHobbits) {
+    case specs:
+        echo "buy specs if you are over 16";
+        break;
+    case mugs:
+        echo "buy mugs if you are over 18";
+        break;
+    case sausagerolls:
+        echo "buy sausagerolls if you are over 21";
+        break;
+    default:
+        echo "wanted goods";
+}
+
+    ?>
+</p>
+</body>
+</html>
